87e4a5efeafda86b62f266a2af2fc46e86a91583
andre2680-source <andre2680@gmail.com>
2026-01-05 12:16:42 +0100

    Create Exercise1B.py

---
+++ python string exercises/Exercise1B.py
@@ -0,0 +1,13 @@
+str1 = "JhonDipPeta"
+
+middle = len(str1) // 2 # Find the middle index
+str1[middle - 1: middle + 2] # Extract 3 middle characters
+res = str1[middle - 1: middle + 2] # Store the result
+print(res)  # Output: 3 middle characters
+
+str2 = "JaSonAy"
+
+middle2 = len(str2) // 2 # Find the middle index
+str2[middle2 - 1: middle2 + 2] # Extract 3 middle characters
+res2 = str2[middle2 - 1: middle2 + 2] # Store the result
+print(res2)  # Output: 3 middle characters
