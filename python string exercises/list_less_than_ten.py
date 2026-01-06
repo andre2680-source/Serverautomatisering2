98650408cb676ebb00d5237473c22bcdc9aad959
andre2680-source <andre2680@gmail.com>
2026-01-06 13:03:45 +0100

    Create list_less_than_ten.py

---
+++ python string exercises/list_less_than_ten.py
@@ -0,0 +1,7 @@
+array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# Print all elements less than 5
+print("Elements less than 5:")
+for element in array:
+    if element < 5:
+        print(element)
