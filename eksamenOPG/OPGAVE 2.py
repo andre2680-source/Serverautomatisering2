156a009f1c4beed986fcfbd522ac6f9c44720c1b
andre2680-source <andre2680@gmail.com>
2026-01-09 10:56:53 +0100

    Create OPGAVE 2.py

---
+++ eksamenOPG/OPGAVE 2.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+key = "sZPSiR6xgy33NJ6udXavhBlK_4k-KCh6aqjLTPlpFxQ="
+F = Fernet(key)
+
+encrypted_text = b"gAAAAABpYLwwrvOG4eZMZeBWccM9l8SMtZ8sBzYPkXD-WNqbN1wscVKpSUjAI1uqaoZEqsfCteFi6jgQiohBrsmVXr4BR1D9YCMFWuOW_-L3qkUmrIEFAFa10esB0s06ROwFbWqqOac6Q4a0Zyl31WqGadQrizrVAgVO4KZbov5-HQPADe5uuCzsdve1sW-GQ-lz9qkoX1w79WtT0ruBhvtcvESqNFP19b3UnXi1UKIZjmYqozspvG-93VuHuROsUDUv8wxRHBl5qcL9sg4rI3haK2RU_sGm1ZA6sY_FQyv2of3OVjXDDcwoadG5oK-w-FcOfhJup4HrsVSM1JqK8PawCNblwZ0VcVonct01czgvgIgTP5C41vq2HGr_h_K5a1Bcmg7n1tKqM19vgq7sSQfdi-v_rbwfkhqXdbGhKfQ-P0505ahRqg6K3eoEKz9fvdm8uUQ6LaS0YjNYgElOT4c_amOAc5-fhpRtqakTp8nBTv8zPQtJ2ck="
+decrypted_text = F.decrypt(encrypted_text)
+print(decrypted_text.decode())
+
+# Envryption key = "sZPSiR6xgy33NJ6udXavhBlK_4k-KCh6aqjLTPlpFxQ="
+
+
+
